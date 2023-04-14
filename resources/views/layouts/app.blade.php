a6777f0621c433dbe9015b4beaff0b63916bbf57
GiuliaGlave <giulia.glave@gmail.com>
2023-04-14 15:18:59 +0200

    add homepage title

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel 9 + Bootstrap Template</title>
+
+  {{-- Includiamo gli assets con la direttiva @vite --}}
+  @vite('resources/js/app.js')
+</head>
+
+<body>
+    @yield('title')
+</body>
+
+</html>
