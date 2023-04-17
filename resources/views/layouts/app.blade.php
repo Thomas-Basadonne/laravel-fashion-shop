4e7f30e72b316d97cee2cad3e2049d4914e94ad1
GiuliaGlave <giulia.glave@gmail.com>
2023-04-17 13:57:30 +0200

    change app title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel 9 + Bootstrap Template</title>
+  <title></title>
   
    {{-- BS Icons --}}
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
