4e3e4cd118e1c283e81a30693224a28a8af634de
Simone Marino <simo.marino@outlook.it>
2023-04-17 14:15:39 +0200

    Home cards + Route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,37 @@
 @extends('layouts.app')
 
-@section('title', 'Homepage')
+@section('title')
     
 @section('content')
-<a href="{{ route('shoes.index') }}">
-  <i class="bi bi-eye-fill"></i>
-</a>
-
+  
+ @forelse($shoes as $shoe)
+    <div class="card-box">
+      <div class="card" style="width: 18rem;">
+        <figure>
+          <img src="{{ $shoe->image }}" alt="" class="img-fluid">
+          <figcaption>Dettagli :</figcaption>
+        </figure>
+        <ul>
+      <li>
+        <p class="card-text"><span class="fw-bold text-primary"> Marca: </span>{{$shoe->marca}}</p>
+      </li>
+      <li>
+        <p class="card-text"><span class="fw-bold text-primary"> Modello: </span>{{$shoe->modello}}</p>
+      </li>
+      <li>
+        <p class="card-text"><span class="fw-bold text-primary"> Colore: </span>{{$shoe->colore}}</p>
+      </li>
+      <li>
+        <p class="card-text"><span class="fw-bold text-primary"> Taglia: </span>{{$shoe->taglia}}</p>
+      </li>
+      <li>
+        <p class="card-text"><span class="fw-bold text-primary"> Prezzo: </span>{{$shoe->prezzo}}€</p>
+      </li>
+    </ul>
+      </div>      
+    </div>
+  @empty
+  @endforelse
 @endsection
+
+
